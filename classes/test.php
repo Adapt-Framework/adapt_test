da96c79a4ff31cc9bc607ca8c3a558b345b0f9f7
Matt Bruton <matt.bruton@gmail.com>
2017-04-28 16:45:42 +0100

    Inital commit

---
+++ classes/test.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace \adapt\test;
+
+/**
+ * Description of test
+ *
+ * @author matt
+ */
+class test extends \adapt\base{
+    
+    public function assert_array_has_key($key, $array, $message = null){
+        
+    }
+    
+    public function assert_class_has_attribute($property_name, $class_name, $message = null){
+        
+    }
+    
+    public function assert_array_subset($subset, $array, $message = null){
+        
+    }
+    
+    public function assert_has_static_attribute($property_name, $class_name, $message = null){
+        
+    }
+    
+    public function assert_contains($needle, $haystack, $message = null){
+        
+    }
+    
+    public function assert_contains_only($type, $haystack, $is_native_type = null, $message = null){
+        
+    }
+    
+    public function assert_contains_only_instances_of($class_name, $haystack, $message = null){
+        
+    }
+    
+    public function assert_count($expected_count, $haystack, $message = null){
+        
+    }
+    
+    public function assert_not_count($expected_count, $haystack, $message = null){
+        
+    }
+    
+    public function assert_directory_exists($directory, $message = null){
+        
+    }
+    
+    public function assert_directory_not_exists($directory, $message = null){
+        
+    }
+    
+    public function assert_directory_is_readable($directory, $message = null){
+        
+    }
+    
+    public function assert_directory_is_not_readable($directory, $message = null){
+        
+    }
+    
+    public function assert_directory_is_writable($directory, $message = null){
+        
+    }
+    
+    public function assert_directory_is_not_writable($directory, $message = null){
+        
+    }
+    
+    public function assert_empty($actual, $message = null){
+        
+    }
+    
+    public function assert_equals_xml_structure($expected_xml, $actual_xml, $check_attributes = false, $message = null){
+        
+    }
+    
+    public function assert_equals($expected, $actual, $message = null){
+        
+    }
+    
+    public function assert_not_equals($expected, $actual, $message = null){
+        
+    }
+    
+    public function assert_false($condition, $message = null){
+        
+    }
+    
+    public function assert_not_false($condition, $message = null){
+        
+    }
+    
+    public function assert_file_equals($expected, $actual, $message = null){
+        
+    }
+    
+    public function assert_file_not_equals($expected, $actual, $message = null){
+        
+    }
+    
+    public function assert_file_exists($filename, $message = null){
+        
+    }
+    
+    public function assert_file_not_exists($filename, $message = null){
+        
+    }
+    
+    public function assert_file_is_readable($filename, $message){
+        
+    }
+    
+    public function assert_file_is_not_readable($filename, $message = null){
+        
+    }
+    
+    public function assert_file_is_writable($filename, $message = null){
+        
+    }
+    
+    public function assert_file_is_not_writable($filename, $message = null){
+        
+    }
+    
+    public function assert_greater_then($expected, $actual, $message = null){
+        
+    }
+}
